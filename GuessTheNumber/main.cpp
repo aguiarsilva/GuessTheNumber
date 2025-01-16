924c7861c27f25e602541d600147b0c9d0e8692d
Brunno Leonardo Aguiar Silva <aguiarsilva@gmail.com>
2025-01-16 19:04:34 +0100

    Optimized code

--- GuessTheNumber/main.cpp
+++ GuessTheNumber/main.cpp
@@ -6,45 +6,40 @@ int main()
 {
 	std::srand(time(nullptr));
 
+	bool guessedNum = false;
 	int input;
 	int number = rand() % 100 + 1;
-	int i = 0;
+	int count = 0;
 
 	std::cout << "Guess the Number Game:" << std::endl;
 	std::cout << "*--------------------*" << std::endl;
-	std::cout << "Please Insert a number to guess the number chosen by the computer:" << std::endl;
-	std::cin >> input;
-	
-	while (i < 100)
+
+	while (!guessedNum)
 	{
+		std::cout << "Please Insert a number to guess the number chosen by the computer:" << std::endl;
+		std::cin >> input;
+
 		if (input < 1 || input > 100)
 		{
 			std::cout << "You must select an integer number between 1 and 100" << std::endl;
-			std::cout << "Please Insert a number to guess the number chosen by the computer:" << std::endl;
-			std::cin >> input;
-			i++;
-		}		
-		else if (input < number)
+			count++;
+			continue;
+		}
+
+		if (input < number)
 		{
 			std::cout << "Your guess is too low!" << std::endl;
-			std::cout << "Please Insert a number to guess the number chosen by the computer:" << std::endl;
-			std::cin >> input;
-			i++;
 		}
-			
 		else if (input > number)
 		{
 			std::cout << "Your guess is too high!" << std::endl;
-			std::cout << "Please Insert a number to guess the number chosen by the computer:" << std::endl;
-			std::cin >> input;
-			i++;
 		}
 		else
 		{
-			std::cout << "Congratulations! You guessed the number in " << i << "! Thanks for playing" << std::endl;
-			break;
+			std::cout << "Congratulations! You guessed the number in " << count << "! Thanks for playing" << std::endl;
+			guessedNum = true;
 		}
-			
+		count++;
 	}
 	return 0;
 }
